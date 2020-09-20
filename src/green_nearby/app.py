6b00cc6cc31c0d1e7ff76f65538ad7c945da601c
Matt Rice <xricexx77@gmail.com>
2020-09-20 18:05:08 -0400

    address submit address butoon

--- src/green_nearby/app.py
+++ src/green_nearby/app.py
@@ -6,7 +6,7 @@ import dash_html_components as html
 import requests
 import pandas
 import plotly.express as px
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 
 URL = "http://localhost:8000/greenspace"
@@ -16,31 +16,37 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 px.set_mapbox_access_token(os.environ["MAPBOX_TOKEN"])
 
-payload = {
- "name": "710 Centre St, Jamaica Plain, MA 02130"
-}
-response = requests.request("POST", URL, json=payload)
-df = pandas.read_json(response.text)
-fig = px.scatter_mapbox(df, text="name", lat="lat", lon="long", zoom=14)
-
-app.layout = html.Div(children=[
-    html.H1(children='Green Nearby'),
-
-    dcc.Input(id="input-address", type="text"),
+app.layout = html.Div([
+    html.H1('Green Nearby'),
+    dcc.Input(id="input-address", type="text", value=''),
+    html.Button(id='submit-button-state', type='submit', n_clicks=0, children='Submit'),
     html.Div(id="out-input"),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='greenspace_map',
+
     )
 ])
 
 @app.callback(
-    Output("out-input", "children"),
-    [Input("input-address", "value")],
+    Output("greenspace_map", "figure"),
+    [Input('submit-button-state', 'n_clicks')],
+    [State("input-address", "value")]
 )
-def address_render(address):
-   return address 
+def address_render(n_clicks, address):
+    print(address)
+    print(n_clicks)
+    if not address:
+        address = "139 Tremont St, Boston, MA 02111"
+    payload = {
+        "name": address
+    }
+    response = requests.request("POST", URL, json=payload)
+    df = pandas.read_json(response.text)
+    fig = px.scatter_mapbox(df, text="name", lat="lat", lon="long", zoom=14)
+    fig.update_layout(transition_duration=500)
+
+    return fig
     
 
 if __name__ == '__main__':
